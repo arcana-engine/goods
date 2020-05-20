8b4cff25010f59fc506d0e89bfad33217fa4afb1
Zakarum <zakarumych@ya.ru>
2020-05-20 10:06:12 +0300

    Fix path to the generated WASM file

--- examples/build-wasm32.bat
+++ examples/build-wasm32.bat
@@ -7,7 +7,7 @@ setlocal
 set RUSTFLAGS=-C linker=lld
 
 cargo build --release --target=wasm32-unknown-unknown --no-default-features --example %* || exit /b 1
-wasm-bindgen --target web --out-dir "%~dp0\generated" --no-typescript "%~dp0\..\..\target\wasm32-unknown-unknown\release\examples\%1.wasm" || exit /b 1
+wasm-bindgen --target web --out-dir "%~dp0\generated" --no-typescript "%~dp0\..\target\wasm32-unknown-unknown\release\examples\%1.wasm" || exit /b 1
 wasm-opt "%~dp0\generated\%1_bg.wasm" -o "%~dp0\generated\%1.wasm" -O2 --disable-threads || exit /b 1
 echo "Success"
 )
