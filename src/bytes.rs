82232b19f1cc1050d0ca09d10d6060b78d566c37
Zakarum <zakarumych@ya.ru>
2020-06-07 10:36:04 +0300

    Asset Arc<[u8]>

--- src/bytes.rs
+++ src/bytes.rs
@@ -17,3 +17,17 @@ impl SimpleAsset for Box<[u8]> {}
 impl<K> AssetDefaultFormat<K> for Box<[u8]> {
     type DefaultFormat = PassthroughFormat;
 }
+
+impl SimpleAsset for alloc::sync::Arc<[u8]> {}
+
+impl<K> AssetDefaultFormat<K> for alloc::sync::Arc<[u8]> {
+    type DefaultFormat = PassthroughFormat;
+}
+
+#[cfg(not(feature = "sync"))]
+impl SimpleAsset for alloc::rc::Rc<[u8]> {}
+
+#[cfg(not(feature = "sync"))]
+impl<K> AssetDefaultFormat<K> for alloc::rc::Rc<[u8]> {
+    type DefaultFormat = PassthroughFormat;
+}
