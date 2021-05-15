1d0823ef7b5aa7eae45f7c98d780f7d9839df8a4
Zakarum <zakarumych@ya.ru>
2021-05-15 19:40:56 +0300

    Impl Asset and AssetContainer for Arc<A>
    And AssetContainer for Arc<[T]>,

--- src/asset.rs
+++ src/asset.rs
@@ -1,6 +1,6 @@
 use {
     crate::loader::Loader,
-    std::{error::Error, future::Future},
+    std::{error::Error, future::Future, sync::Arc},
 };
 
 /// An asset type that can be built from decoded representation.
@@ -43,4 +43,98 @@ pub trait AssetContainer: Clone + Sized + Send + Sync + 'static {
 pub trait AssetContainerBuild<B>: AssetContainer {
     /// Build asset instance using decoded representation and `Resources`.
     fn build(decoded: Self::Decoded, builder: &mut B) -> Result<Self, Self::Error>;
+}
+
+impl<A> Asset for Arc<A>
+where
+    A: Asset,
+{
+    type Error = A::Error;
+    type Decoded = A::Decoded;
+    type Fut = A::Fut;
+
+    fn decode(bytes: Box<[u8]>, loader: &Loader) -> Self::Fut {
+        A::decode(bytes, loader)
+    }
+}
+
+impl<A, B> AssetBuild<B> for Arc<A>
+where
+    A: AssetBuild<B>,
+{
+    fn build(decoded: Self::Decoded, builder: &mut B) -> Result<Self, Self::Error> {
+        A::build(decoded, builder).map(Arc::new)
+    }
+}
+
+impl<A> AssetContainer for Box<A>
+where
+    A: AssetContainer,
+{
+    type Info = A::Info;
+    type Error = A::Error;
+    type Decoded = A::Decoded;
+    type Fut = A::Fut;
+
+    fn decode(info: A::Info, loader: &Loader) -> Self::Fut {
+        A::decode(info, loader)
+    }
+}
+
+impl<A, B> AssetContainerBuild<B> for Box<A>
+where
+    A: AssetContainerBuild<B>,
+{
+    fn build(decoded: A::Decoded, builder: &mut B) -> Result<Self, Self::Error> {
+        A::build(decoded, builder).map(Box::new)
+    }
+}
+
+impl<A> AssetContainer for Arc<A>
+where
+    A: AssetContainer,
+{
+    type Info = A::Info;
+    type Error = A::Error;
+    type Decoded = A::Decoded;
+    type Fut = A::Fut;
+
+    fn decode(info: A::Info, loader: &Loader) -> Self::Fut {
+        A::decode(info, loader)
+    }
+}
+
+impl<A, B> AssetContainerBuild<B> for Arc<A>
+where
+    A: AssetContainerBuild<B>,
+{
+    fn build(decoded: A::Decoded, builder: &mut B) -> Result<Self, Self::Error> {
+        A::build(decoded, builder).map(Arc::new)
+    }
+}
+
+impl<A> AssetContainer for Arc<[A]>
+where
+    A: AssetContainer,
+{
+    type Info = Vec<A::Info>;
+    type Error = A::Error;
+    type Decoded = Vec<A::Decoded>;
+    type Fut = futures::future::TryJoinAll<A::Fut>;
+
+    fn decode(infos: Vec<A::Info>, loader: &Loader) -> Self::Fut {
+        futures::future::try_join_all(infos.into_iter().map(|info| A::decode(info, loader)))
+    }
+}
+
+impl<A, B> AssetContainerBuild<B> for Arc<[A]>
+where
+    A: AssetContainerBuild<B>,
+{
+    fn build(decoded: Vec<A::Decoded>, builder: &mut B) -> Result<Self, Self::Error> {
+        decoded
+            .into_iter()
+            .map(|decoded| A::build(decoded, builder))
+            .collect()
+    }
 }
